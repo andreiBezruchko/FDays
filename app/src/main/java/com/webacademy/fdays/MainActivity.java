7bad47cb4af428767d2d8656b785e9b6be764bb2
sharikd <sharikd@gmail.com>
2016-02-19 00:14:23 +0200

    Добавил разметку приложения и фрагменты. Пока некрасиво, красиво сделаю завтра вечером или уже в субботу.

--- app/src/main/java/com/webacademy/fdays/MainActivity.java
+++ app/src/main/java/com/webacademy/fdays/MainActivity.java
@@ -1,14 +1,46 @@
 package com.webacademy.fdays;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager vpPager = (ViewPager) findViewById(R.id.pager);
+        adapterViewPager = new CustomPagerAdapter.MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+        vpPager.getCurrentItem();
+
+
+        vpPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            // This method will be invoked when a new page becomes selected.
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(MainActivity.this,
+                        "Selected page position: " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            // This method will be invoked when the current page is scrolled
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Code goes here
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Code goes here
+            }
+        });
 
     }
 }
